3dc7e3108de8a135092fe79badb7ebd86300e394
Raoul Jaeckel <rja@auctionata.com>
2016-02-02 15:34:13 +0100

    use total value as page limit in case it is greater

--- dist/angular-simple-pagination.js
+++ dist/angular-simple-pagination.js
@@ -20,7 +20,7 @@ function SimplePagination() {
   };
 }
 
-function SimplePaginationController() {
+function SimplePaginationController($scope) {
   var self = this;
 
   self.currentPage = self.currentPage || 0;
@@ -58,4 +58,12 @@ function SimplePaginationController() {
       self.onUpdate();
     }
   }
+
+  $scope.$watch(function () {
+    return self.total;
+  }, function (newTotal) {
+    if (newTotal && self.pageLimit > newTotal) {
+      self.pageLimit = newTotal;
+    }
+  });
 }
